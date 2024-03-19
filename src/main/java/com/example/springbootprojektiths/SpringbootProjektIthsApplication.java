b397ddc6bc16d926ffd93e051dae57b19bcfa454
Yrla Hackzell <yrla.hackzell@hotmail.com>
2024-03-19 12:53:45 +0100

    first commit

---
+++ src/main/java/com/example/springbootprojektiths/SpringbootProjektIthsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootprojektiths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjektIthsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProjektIthsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootprojektiths/SpringbootProjektIthsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootprojektiths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProjektIthsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
