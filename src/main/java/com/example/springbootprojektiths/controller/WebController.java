22bb277a4bff2ee8e198ccca31136ba6d98f84dc
August <august.rydnell@gmail.com>
2024-03-20 13:26:07 +0100

    adding data to database Co-authored-by: August Rydnell <august.ryd@gmail.com> Co-authored-by: Tobias Sjöström tobias.sjostrom@iths.se> Co-authored-by: Jens Nilsson <jn.jensnilsson@gmail.com> Co-authored-by: Hanna Stigland <hanna.stigland@iths.se>

--- src/main/java/com/example/springbootprojektiths/controller/WebController.java
+++ src/main/java/com/example/springbootprojektiths/controller/WebController.java
@@ -2,11 +2,12 @@ package com.example.springbootprojektiths.controller;
 
 import com.example.springbootprojektiths.entity.Message;
 import com.example.springbootprojektiths.repository.MessageRepository;
+import com.example.springbootprojektiths.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import com.example.springbootprojektiths.entity.User;
 import java.util.List;
 
 @Controller
@@ -15,9 +16,13 @@ public class WebController {
     @Autowired
     MessageRepository messageRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/posts")
     String posts(Model model ){
-        List<Message> messages = messageRepository.findAll();
+       List<Message> messages = messageRepository.findAll();
+       // List<User> people = userRepository.findAll();
         model.addAttribute("messages", messages);
         return "posts";
     }
