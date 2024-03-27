c8db005a663f303e07fd4160732ea64ae3d75e59
Yrla Hackzell <yrla.hackzell@hotmail.com>
2024-03-27 16:16:05 +0100

    add oauth2 to view
    Co-authored-by: Tobias Sjöström <tobias.sjostrom@iths.se>
    Co-authored-by: Hanna Stigland <hanna.stigland@iths.se>
    Co-authored-by: August Rydnell <august.ryd@gmail.com>
    Co-authored-by: Jens Nilsson <jn.jensnilsson@gmail.com>

--- src/main/java/com/example/springbootprojektiths/entity/User.java
+++ src/main/java/com/example/springbootprojektiths/entity/User.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // @GeneratedValue(strategy = GenerationType.IDENTITY) // behövs inte för github löser d
     private Long id;
     private String fullName;
     private String loginName;
