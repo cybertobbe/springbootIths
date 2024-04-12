cf0321ffdce7f7c5f16aa3e676bbb4111ba1275f
cybertobbe <143131053+cybertobbe@users.noreply.github.com>
2024-04-12 14:59:16 +0200

    Add comment as a note before first start

--- src/main/java/com/example/springbootprojektiths/SpringbootProjektIthsApplication.java
+++ src/main/java/com/example/springbootprojektiths/SpringbootProjektIthsApplication.java
@@ -1,5 +1,8 @@
 package com.example.springbootprojektiths;
 
+//Before first run.
+//Add CLIENT_ID and CLIENT_SECRET to enviraonment variablesin run configuration.
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
